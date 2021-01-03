e602fbf8af26d551c491a31a08d85144ad685d66
Jacob Sommer <jacob.sommer@comcast.net>
2021-01-03 13:09:09 -0800

    Functional home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class Home extends React.Component {
+
+interface IState {
+  code: number
+}
+
+class Home extends React.Component<{}, IState> {
+  readonly state: IState = { code: Number.NaN };
+
+  handleCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ code: Number.parseInt(e.target.value) });
+  }
+
   render() {
     return(
       <div className="title-screen">
         <div className="wrapper">
           <h1>Hangman</h1>
-          <Link className="button" to="/room">Create Room</Link>
+          <Link className="button" to="/create-room">Create Room</Link>
           <div className="join-group">
-            <input type="text" name="code" />
-            <a className="button" href="#">Join</a>
+            <input type="number" name="code" min={0} max={9999} value={this.state.code} onChange={this.handleCodeChange} />
+            <Link className="button" to={`/room/${this.state.code}`}>Join</Link>
           </div>
         </div>
       </div>
