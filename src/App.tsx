69c425db82cc2e63ae331e0b7b14c1df7a631a18
Jacob Sommer <jacob.sommer@comcast.net>
2021-01-03 13:09:49 -0800

    create room routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Room from './components/Room';
+import CreateRoom from './components/CreateRoom';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/room/:id" component={Room} />
-        <Route path="/room" component={Room} />
+        <Route path="/create-room" component={CreateRoom} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
