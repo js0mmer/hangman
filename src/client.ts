846a9523be84d5ac903706f894890534c60e5c77
Jacob Sommer <jacob.sommer@comcast.net>
2021-01-05 14:55:19 -0800

    Adjust server url

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import socketIO from 'socket.io-client';
 
-const serverURL = 'https://js0mmer-hangman.herokuapp.com:4000';
+const serverURL = 'https://js0mmer-hangman.herokuapp.com/';
 const socket = socketIO(serverURL);
 
 export default socket;
