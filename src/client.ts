deb7b8debfb6602c8733eeb976ed84c008e10d5a
Jacob Sommer <jacob.sommer@comcast.net>
2021-01-05 17:20:23 -0800

    Fix for dev env

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import socketIO from 'socket.io-client';
 
-const serverURL = 'https://js0mmer-hangman.herokuapp.com/';
+const serverURL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://js0mmer-hangman.herokuapp.com/';
 const socket = socketIO(serverURL);
 
 export default socket;
